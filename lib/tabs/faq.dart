b239ba568688083ccaa649cfc0e15a513e9c0633
Max Tang <maxtang2001@gmail.com>
2022-04-16 02:33:44 -0400

    minor UI changes.

--- lib/tabs/faq.dart
+++ lib/tabs/faq.dart
@@ -7,7 +7,7 @@ AlertDialog faq (context) {
   return AlertDialog(
     title: const Center(child: Text("FAQ")),
     content: SizedBox(
-        width: MediaQuery.of(context).size.width / 4,
+        width: MediaQuery.of(context).size.width / 2.5,
         height: MediaQuery.of(context).size.height / 2,
         child: FutureBuilder(
             future: rootBundle.loadString("assets/data.json"),
@@ -20,40 +20,44 @@ AlertDialog faq (context) {
   );
 }
 
-Widget recursiveList (content) {
-  if (content is List) {
-      return ListView.builder(
-        shrinkWrap: true,
-        itemCount: content.length,
-        itemBuilder: (context, index) {
-          if (content[index].containsKey("Question")) {
-            return ExpansionTile(
-              textColor: Colors.black,
-              title: Text(content[index]["Question"]),
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(content[index]["Answer"]),
-                )
-              ],
-            );
-          }
+  Widget recursiveList (content) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: content.length,
+      itemBuilder: (context, index) {
+        if (content[index].containsKey("Question")) {
           return ExpansionTile(
             textColor: Colors.black,
-            title: Text(content[index]["Title"]),
-            children: [ListTile(subtitle: recursiveList(content[index]["Content"]))],
+            title: Text(content[index]["Question"]),
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  title: Text(
+                      content[index]["Answer"],
+                      style: const TextStyle(
+                          fontSize: 15,
+                          fontStyle: FontStyle.italic
+                      )
+                  ),
+                ),
+              )
+            ],
           );
+        }
+        return ExpansionTile(
+          textColor: Colors.black,
+          title: Text(
+              content[index]["Title"],
+              style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+              )
+          ),
+          children: [ListTile(
+              subtitle: recursiveList(content[index]["Content"])
+          )],
+        );
       },
     );
-  }
-  return ExpansionTile(
-    textColor: Colors.black,
-    title: Text(content["Question"]),
-    children: [
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(content["Answer"]),
-      )
-    ]
-  );
 }
