79e94824863c2a5a03007766faffe5fd7879555b
Max Tang <maxtang2001@gmail.com>
2022-04-16 21:55:02 -0400

    changed QA expansion tile to a statefulwidget.

--- lib/tabs/faq.dart
+++ lib/tabs/faq.dart
@@ -20,44 +20,69 @@ AlertDialog faq (context) {
   );
 }
 
-  Widget recursiveList (content) {
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: content.length,
-      itemBuilder: (context, index) {
-        if (content[index].containsKey("Question")) {
-          return ExpansionTile(
-            textColor: Colors.black,
-            title: Text(content[index]["Question"]),
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ListTile(
-                  title: Text(
-                      content[index]["Answer"],
-                      style: const TextStyle(
-                          fontSize: 15,
-                          fontStyle: FontStyle.italic
-                      )
-                  ),
-                ),
-              )
-            ],
-          );
-        }
-        return ExpansionTile(
-          textColor: Colors.black,
-          title: Text(
-              content[index]["Title"],
-              style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold
-              )
+Widget recursiveList (content) {
+  return ListView.builder(
+    shrinkWrap: true,
+    itemCount: content.length,
+    itemBuilder: (context, index) {
+      if (content[index].containsKey("Question")) {
+        return QATile(content: content[index]);
+      }
+      return ExpansionTile(
+        textColor: Colors.black,
+        title: Text(
+            content[index]["Title"],
+            style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+            )
+        ),
+        children: [ListTile(
+            subtitle: recursiveList(content[index]["Content"])
+        )],
+      );
+    },
+  );
+}
+
+class QATile extends StatefulWidget {
+  final content;
+  const QATile({Key? key, required this.content}) : super(key: key);
+
+  @override
+  _QATile createState() => _QATile();
+}
+
+class _QATile extends State<QATile> {
+  bool _selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      textColor: Colors.black,
+      title: Text(
+          widget.content["Question"],
+          style: TextStyle(
+            color: _selected ? Colors.blue : Colors.black
           ),
-          children: [ListTile(
-              subtitle: recursiveList(content[index]["Content"])
-          )],
-        );
+      ),
+      onExpansionChanged: (expanded) {
+        setState(() { _selected = expanded; });
       },
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            title: Text(
+                widget.content["Answer"],
+                style: const TextStyle(
+                    fontSize: 15,
+                    fontStyle: FontStyle.italic
+                )
+            ),
+          ),
+        )
+      ],
     );
+  }
 }
